644768a3f73c3541f62d3133d4cc6e0a1affc76b
Rick Love <rick@toldpro.com>
2017-07-02 17:33:03 -0500

    FIX Feedback button

--- src-client/src/app/components/smart/feedbackButton.tsx
+++ src-client/src/app/components/smart/feedbackButton.tsx
@@ -2,7 +2,7 @@ import * as RX from 'reactxp';
 import { colors, styles } from '../../styles';
 import { FeedbackIcon } from "../common/icons/feedback";
 
-const formLinkUrl = 'https://goo.gl/forms/FORM_URL?';
+const formLinkUrl = 'https://goo.gl/forms/UqDy0bnUMON0T2ny1';
 
 const style = { ...styles.postIcon_action, fillColor: colors.actionIcons.e_yellow };
 
